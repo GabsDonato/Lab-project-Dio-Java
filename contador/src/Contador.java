8a12d1c0873cf16b9025b4e4ddb4f0edaef05018
Gabriela donato <146032843+GabsDonato@users.noreply.github.com>
2025-02-25 11:22:57 -0300

    finalizado contador

--- contador/src/Contador.java
+++ contador/src/Contador.java
@@ -1,4 +1,34 @@
+import java.util.Scanner;
 
 public class Contador {
-    
+
+    public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in);
+        System.out.println("Digite o primeiro parâmetro: ");
+        int parametroUm = terminal.nextInt();
+        System.out.println("Digite o segundo parâmetro: ");
+        int parametroDois = terminal.nextInt();
+
+        try {
+            contar(parametroUm, parametroDois);
+        } catch (ParametroInvalidosException e) {
+            System.out.println("O primeiro parametro deve ser maior que o segundo: " + e.getMessage());
+        }
+    }
+
+    static void contar(int parametroUm, int parametroDois) throws ParametroInvalidosException {
+        if (parametroUm > parametroDois) {
+            throw new ParametroInvalidosException("O primeiro parametro deve ser maior que o segundo");
+        }
+        int contagem = parametroDois - parametroUm;
+        for (int i = 0; i <= contagem; i++) {
+            System.out.println(parametroUm + i);
+        }
+    }
+}
+
+class ParametroInvalidosException extends Exception {
+    public ParametroInvalidosException(String message) {
+        super(message);
+    }
 }
